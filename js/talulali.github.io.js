fda8ce887afcbd318506e5695dd79284741df847
Tali Lavi <tali@lavi.fm>
2015-03-20 16:27:06 +0000

    Final website., for submission.

--- js/talulali.github.io.js
+++ js/talulali.github.io.js
@@ -2,7 +2,11 @@
  * Created by admin on 20/03/15.
  */
 function displayScenarioDetails(buttonId) {
+    $("#scenarioNumber").html("Scenario #"+buttonId);
     $("#title").html(scenarios[buttonId-1].scenarioTitle);
     $("#content").html(scenarios[buttonId-1].scenarioContent);
-    $("#diagram-img").attr("src","images/"+buttonId+".jpg");
+    //$("#diagram-img").attr("src","images/"+buttonId+".jpg");
+    $("#diagram-img").html("<img class='img-responsive' src='images/"+buttonId+".jpg' alt='Entity Diagram'>");
 }
+
+

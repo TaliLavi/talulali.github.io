09941dfe66ff3026bc355993119440943f15a215
Tali Lavi <tali@lavi.fm>
2015-03-20 11:57:01 +0000

    Added DOM manipulation for image.

--- js/talulali.github.io.js
+++ js/talulali.github.io.js
@@ -4,6 +4,5 @@
 function displayScenarioDetails(buttonId) {
     $("#title").html(scenarios[buttonId-1].scenarioTitle);
     $("#content").html(scenarios[buttonId-1].scenarioContent);
-    //document.getElementById("title").innerHTML = scenarios[0].scenarioTitle;
-    //document.getElementById("content").innerHTML = scenarios[buttonId].scenarioContent;
+    $("#diagram-img").attr("src","images/"+buttonId+".jpg");
 }
